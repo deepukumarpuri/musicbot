e9d400474f3d2202b63c1b4c5ae00c471a678b2d
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-15 07:18:48 +0530

    Update Start.py

--- Yukki/Plugins/Start.py
+++ Yukki/Plugins/Start.py
@@ -75,17 +75,77 @@ async def welcome(_, message: Message):
             return
 
 
-@app.on_message(filters.command(["help", "start"]) & filters.group)
-@PermissionCheck
-async def useradd(_, message: Message):
+@Client.on_message(filters.group & filters.command(["start", "help"]))
+async def start(_, message: Message):
+    chat_id = message.chat.id
+    if not await is_served_chat(chat_id):
+        await message.reply_text(f"**__Not in allowed chats.__**\n\nMusic Private is only for allowed chats. Ask any Sudo User to allow your chat.\nCheck Sudo Users List [From Here](https://t.me/{BOT_USERNAME}?start=sudolist)")
+         return await app.leave_chat(chat_id)
     out = start_pannel()
-    await asyncio.gather(
-        message.delete(),
-        message.reply_text(
-            f"Thanks for having me in {message.chat.title}.\n{MUSIC_BOT_NAME} is alive.\n\nFor any assistance or help, checkout our support group and channel.",
-            reply_markup=InlineKeyboardMarkup(out[1]),
-        ),
-    )
+    await message.reply_text(f"Thanks for having me in {message.chat.title}.\nMusic is alive.\n\nFor any assistance or help, checkout our support group and channel.", reply_markup=InlineKeyboardMarkup(out[1]))
+    return
+        
+@Client.on_message(filters.private & filters.incoming & filters.command("start"))
+async def play(_, message: Message):
+    if len(message.command) == 1:
+        user_id = message.from_user.id
+        user_name = message.from_user.first_name
+        rpk = "["+user_name+"](tg://user?id="+str(user_id)+")" 
+        await app.send_message(message.chat.id,
+            text=f"Hello {rpk}!\n\nThis is AnieMusic Bot.\nI play music on Telegram's Voice Chats.\n\nOnly for selected chats.",
+            parse_mode="markdown",
+            reply_markup=pstart_markup,
+            reply_to_message_id=message.message_id
+        )
+    elif len(message.command) == 2:                                                           
+        query = message.text.split(None, 1)[1]
+        f1 = (query[0])
+        f2 = (query[1])
+        f3 = (query[2])
+        finxx = (f"{f1}{f2}{f3}")
+        if str(finxx) == "inf":
+            query = ((str(query)).replace("info_","", 1))
+            query = (f"https://www.youtube.com/watch?v={query}")
+            with yt_dlp.YoutubeDL(ytdl_opts) as ytdl:
+                x = ytdl.extract_info(query, download=False)
+            thumbnail = (x["thumbnail"])
+            searched_text = f"""
+🔍__**Video Track Information**__
+❇️**Title:** {x["title"]}
+   
+⏳**Duration:** {round(x["duration"] / 60)} Mins
+👀**Views:** `{x["view_count"]}`
+👍**Likes:** `{x["like_count"]}`
+👎**Dislikes:** `{x["dislike_count"]}`
+⭐️**Average Ratings:** {x["average_rating"]}
+🎥**Channel Name:** {x["uploader"]}
+📎**Channel Link:** [Visit From Here]({x["channel_url"]})
+🔗**Link:** [Link]({x["webpage_url"]})
+⚡️ __Searched Powered By Anir Music Bot__"""
+            link = (x["webpage_url"])
+            buttons = personal_markup(link)
+            userid = message.from_user.id
+            thumb = await down_thumb(thumbnail, userid)
+            await app.send_photo(message.chat.id,
+                photo=thumb,                 
+                caption=searched_text,
+                parse_mode="markdown",
+                reply_markup=InlineKeyboardMarkup(buttons),
+            )
+        if str(finxx) == "sud":
+            sudoers = await get_sudoers()
+            text = "**__Sudo Users List of Music:-__**\n\n"
+            for count, user_id in enumerate(sudoers, 1):
+                try:                     
+                    user = await app.get_users(user_id)
+                    user = user.first_name if not user.mention else user.mention
+                except Exception:
+                    continue                     
+                text += f"➤ {user}\n"
+            if not text:
+                await message.reply_text("No Sudo Users")  
+            else:
+                await message.reply_text(text)
 
 
 @app.on_message(filters.command("settings") & filters.group)
